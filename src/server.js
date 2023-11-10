8e62cec8fd9fb0f0b73e09c56052d9bc52f54135
Al3xis <alexxxfernandez50@gmail.com>
2023-11-10 00:39:24 -0300

    someUpdates

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const methodOverride = require('method-override');
 const flash = require('connect-flash');
 const session = require('express-session');
 const passport = require('passport');
-
+const dontenv = require('dotenv');
 const app = express();
 require('./config/passport');
 
